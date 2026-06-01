aa7dd3910edc9f86f5f82610da77a3a75189f83c
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-01 16:02:03 +0900

    [done] exo3

---
+++ dao-python-exercises/doa-ex03.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = [int(x) for x in input().split()]
+
+index_of_even = 0
+for i in range(n):
+    if a[i] % 2 == 0:
+        index_of_even = i + 1      
+        break   
+print(index_of_even)
