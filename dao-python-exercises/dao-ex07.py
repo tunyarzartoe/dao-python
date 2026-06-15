bdd63111eacd907087dac0c0e26f77266440b079
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-15 14:26:06 +0900

    [done] dao-ex07

---
+++ dao-python-exercises/dao-ex07.py
@@ -0,0 +1,25 @@
+def print_array(a):
+    print(*a)
+
+
+def selection_sort(a, n):
+    for i in range(n-1):
+        # 変数 min_index を用意する
+        min_index = i
+
+        # j が i+1 から n-1 までのループを用意する
+        for j in range(i+1, n):
+            # A_j < A_{min_index} なら
+            if a[j] < a[min_index]:
+                # min_index を j に更新する
+                min_index = j
+
+        # A_i と A_{min_index} を交換する
+        a[i], a[min_index] = a[min_index], a[i]
+
+        print_array(a)
+
+
+n = int(input())
+a = [int(x) for x in input().split()]
+selection_sort(a, n)
